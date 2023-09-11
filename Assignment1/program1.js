9b2d0a8c6f7de78e1e759cb9acec7d4896df8fa2
Peiyu Huang <w0472206@nscc.ca>
2023-09-11 19:35:03 -0300

    added $ & checked rubric

--- Assignment1/program1.js
+++ Assignment1/program1.js
@@ -1,5 +1,5 @@
 /*
-Author: Peiyu HUang
+Author: Peiyu Huang
 Date: 
 Description: 
 */
@@ -9,20 +9,23 @@ var readlineSync = require("readline-sync");
 // DO NOT EDIT: The main function to house our program code 
 function main()
 {
-    console.log("Hipster’s Local Vinyl Records - Customer Order Details");
+  
     //input
-  var custoName = readlineSync.question("Enter customer's name:");
-  var distance = readlineSync.question("Enter the distance in Kilometers for delivery:");
-  var cost = readlineSync.question("Enter the cost of cost of records purchased:");
+    console.log("Hipster’s Local Vinyl Records - Customer Order Details");
+    var custoName = readlineSync.question("Enter customer's name: ");
+    var distance = readlineSync.question("Enter the distance in Kilometers for delivery: ");
+    var cost = readlineSync.question("Enter the cost of cost of records purchased: ");
+
     //process
     var deliverCost = distance *15;
-    var purchaseCost = cost *1.14;
+    var purchaseCost = cost *1.14; //plus tax
     var total = deliverCost + purchaseCost;
+
     //output
-    console.log("Purchase summary for Joe Cool");
-    console.log("Delivery Cost:" + deliverCost);
-    console.log("Purchase Cost:" + purchaseCost);
-    console.log("Total Cost   :" + total);
+    console.log("Purchase summary for " + custoName);
+    console.log("Delivery Cost:" + " $ " + deliverCost);
+    console.log("Purchase Cost:" + " $ " + purchaseCost);
+    console.log("Total Cost   :" + " $ " + total);
 
 }
 
