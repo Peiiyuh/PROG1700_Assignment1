20e0db097346712aa3f27e5d846676a32c3c7e8d
Peiyu Huang <w0472206@nscc.ca>
2023-09-12 17:19:23 -0300

    added simple code

--- Assignment1/program2.js
+++ Assignment1/program2.js
@@ -4,13 +4,25 @@ Date:
 Description: 
 */
 "use strict";
+var readlineSync = require("readline-sync");
 
 // DO NOT EDIT: The main function to house our program code 
 function main()
 {
     //input
+    console.log("Weekly Loan Caculator\n");
+    var amouLoan = readlineSync.question("Enter the amount of loan: ");
+    var inteRate = readlineSync.question("Enter the interest rate (%): ");
+    var numyears = readlineSync.question("Enter the number of years: ");
+    console.log('  ')
+
     //process
+
+
     //output
+    console.log("Your weekly payment will be: ");
+
+
 }
 
 // DO NOT EDIT: Trigger our main function to launch the program
