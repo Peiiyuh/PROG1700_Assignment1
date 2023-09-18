f8f65e8f26ceacc544ab299a3532e522c2c7dccb
Peiyu Huang <w0472206@nscc.ca>
2023-09-17 23:07:59 -0300

    final commit upgrade coding and add note

--- Assignment1/program3.js
+++ Assignment1/program3.js
@@ -10,23 +10,26 @@ var readlineSync = require("readline-sync");
 // DO NOT EDIT: The main function to house our program code 
 function main()
 {
-    //input
     console.log("Imperial To Metric Conversion\n");
-    var numTon = readlineSync.question("Enter the numebr of tons: ");
-    var numSto = readlineSync.question("Enter the number of stone: ");
-    var numLB = readlineSync.question("Enter the number of pounds: ");
-    var numOns = readlineSync.question("Enter the number of ounces: ");
+    //input
+    //add parseInt to take string return to number!!
+    var numTon = parseInt(readlineSync.question("Enter the numebr of tons: "));
+    var numSto = parseInt(readlineSync.question("Enter the number of stone: "));
+    var numLB = parseInt(readlineSync.question("Enter the number of pounds: "));
+    var numOns = parseInt(readlineSync.question("Enter the number of ounces: "));
     console.log("    ")
 
-    //process
-    var talOns = numTon*35840 + numSto*224 + numLB*16 + numOns*1; //should make it as math method otherwise apear erros
-    var talKilo = talOns/35.274; //ex:5/20/2/4 5208.25537
-    var metTon = parseInt(talKilo/1000); //5
-    var kilos = parseInt(talKilo%1000); //208
-    //var grams = ((talKilo % 1)*1000).toFixed(1); another opt
+    //proces
+    //example: 5.20.2.4 --5208.25537
+    var talOns = numTon * 35840 + numSto * 224 + numLB * 16 + numOns; 
+    var talKilo = talOns / 35.274; 
+    var metTon = parseInt(talKilo / 1000); 
+    var kilos = parseInt(talKilo % 1000); 
+    // original: ((talKilo-parseInt(talKilo))*1000).toFixed(1)
+    var grams = ((talKilo % 1)*1000).toFixed(1); 
 
     //output
-    console.log("The metric weight is " + metTon + " metric tons, " + kilos + " kilos, and " + ((talKilo-parseInt(talKilo))*1000).toFixed(1) + " grams.");
+    console.log(`The metric weight is ${metTon} metric tons, ${kilos} kilos, and ${grams} grams.`);
 }
 
 // DO NOT EDIT: Trigger our main function to launch the program
